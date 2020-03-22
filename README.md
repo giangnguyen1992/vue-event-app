bb37e771a942836d80e18fb0eecf617106f821ba
Giang Nguyen <giang.nguyen@spiegel.de>
2020-03-22 17:38:23 +0100

    finished project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# real-life-project
+# real-world-vue
 
 ## Project setup
 ```
@@ -19,6 +19,3 @@ npm run build
 ```
 npm run lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/EventList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></EventCard>
+    <p></p>
+  </div>
+</template>
+
+<script>
+import EventService from '../services/EventService'
+import EventCard from '../components/Eventcard'
+
+export default {
+  components: {
+    EventCard
+  },
+  created() {
+    EventService.getEvents('/events')
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:', error)
+      })
+  },
+  data() {
+    return {
+      events: []
+    }
+  }
+}
+</script>
